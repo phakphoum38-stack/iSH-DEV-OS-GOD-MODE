545129934d5f7e8078f60859c4a53776ac259507
phakphoum38-stack <phakphoum38@gmail.com>
2026-03-24 23:08:36 +0700

    Create boot.sh

---
+++ core/boot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+
+echo "===================================="
+echo "     iSH DEV OS - GOD MODE"
+echo "===================================="
+
+sleep 1
+
+bash core/system_check.sh
+bash core/install_packages.sh
+bash core/dev_menu.sh
